f06c75df30f560e3c3cf0c5b6f94bd7315f4d41b
Martin VALLEE <martin.vallee@gisp.local>
2026-04-22 16:46:03 +0200

    feat(client): configuration du routage et du service api pour les annonces

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import PrivateHomePage from "./pages/PrivateHomePage";
+import CreateAnnonce from "./pages/CreateAnnonce";
 import { useAuth } from "./context/AuthContext";
 import ProfilePage from "./pages/ProfilePage";
+import MyAnnoncesPage from "./pages/MyAnnoncesPage";
+import AnnonceDetailPage from "./pages/AnnonceDetailPage";
 
 function ProtectedRoute({ children }) {
   const { isAuthenticated, loading } = useAuth();
 
-  if (loading) return <div className="center-loader">Chargement...</div>;
+  if (loading) return <div className="center-loader">Chargement en cours...</div>;
   if (!isAuthenticated) return <Navigate to="/connexion" replace />;
   return children;
 }
@@ -20,6 +23,23 @@ export default function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/connexion" element={<LoginPage />} />
       <Route path="/inscription" element={<RegisterPage />} />
+      <Route path="/annonces/:id" element={<AnnonceDetailPage />} />
+      <Route
+        path="/creer-annonce"
+        element={
+          <ProtectedRoute>
+            <CreateAnnonce />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/mes-annonces"
+        element={
+          <ProtectedRoute>
+            <MyAnnoncesPage />
+          </ProtectedRoute>
+        }
+      />
       <Route
         path="/profil"
         element={
